c1db129ce69ba08b036daca89484998eaceb94c9
philkoerber <philipp.koerber.93@gmail.com>
2023-06-23 15:45:22 +0900

    add spinner component for lobby screen

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import Leaderboard from './LobbyComponents/Leaderboard';
 import Axios from 'axios';
 import GameButtons from './LobbyComponents/GameButtons';
 import useFistStore from './fistStore';
+import Spinner from './LobbyComponents/Spinner';
 
-const boxStyles = "bg-darkblue"
+
+const parentBoxProps = {
+  className: "bg-darkblue"
+}
 
 function Lobby(props) {
 
-    const [playersList, setPlayersList] = useState([])
+    const [playersList, setPlayersList] = useState(null)
     const [user, setUser] = useState(null)
     const [loadingButton, setLoadingButton] = useState(false) //true if looking for game
 
     const { socket, connectToSocket, disconnectSocket, setGameId } = useFistStore();
 
+  const boxProps = {
+      className: "flex w-full h-full justify-center items-center",
+      initial: { opacity: 0 },
+      animate: {opacity: 1}
+
+      }
+  
     useEffect(() => {
     Axios({
       method: "post",
@@ -37,26 +49,55 @@ function Lobby(props) {
       url: "http://localhost:3001/findGame",
       data: { socketId: socket.id },
     }).then((res) => {
+      console.log(res)
       setLoadingButton(!res.data.foundGame);
     });
     };
 
     return (
-        <div className='h-full w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4'>
-            <div className={boxStyles + ''}>
-                <GameButtons
+      <div className='h-full w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4'>
+        <div {...parentBoxProps}>
+          <motion.div
+          {...boxProps}
+          key={user}>
+          {user ?
+            <GameButtons
                     user={user}
                     handleGameButton={() => handleGameButton()}
                     loadingButton={loadingButton} />
-            </div>
-            <div className={boxStyles + ''}>
-                <Leaderboard playersList={playersList} />
-            </div>
-            <div className={boxStyles + ''}>
+            :
+            <Spinner/>
+                }
                 
+            </motion.div>
+        </div>
+        
+        <div {...parentBoxProps}>
+                <motion.div
+          {...boxProps}
+          key={playersList}
+        >
+          {playersList ?
+            <Leaderboard playersList={playersList} />
+            :
+            <Spinner/>
+                }
+                
+            </motion.div>
+        </div>
+
+        <div {...parentBoxProps}>
+                <motion.div {...boxProps}>
+                <Spinner/>
+            </motion.div>
+        </div>
+        
+        <div {...parentBoxProps}>
+          <motion.div {...boxProps}>
+                <Spinner/>
+            </motion.div>
             </div>
-            <div className={boxStyles + ''}>
-            </div>
+        
         </div>
     );
 }
---
+++ client/src/components/LobbyComponents/Spinner.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+function Spinner(props) {
+
+
+    return (
+        <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1, repeat: Infinity, ease: 'linear' }}>
+            <motion.div
+        className="border-1 border-t-2 rounded-full h-16 w-16"
+                style={{
+            borderTopColor: "#4BA3C3",
+          borderRightColor: 'transparent',
+          borderBottomColor: 'transparent',
+            borderLeftColor: 'transparent',
+        
+        }}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1+Math.random(), repeat: Infinity, ease: 'backInOut' }}
+      ></motion.div>
+      </motion.div>
+      
+  );
+}
+
+export default Spinner;
